e08b5fe437ced100b2c153a0288550d22383c098
DaffaJatmiko <daffamiko123@gmail.com>
2024-07-12 17:53:18 +0700

    edit config

--- config/config.go
+++ config/config.go
@@ -3,14 +3,15 @@ package config
 import (
 	"github.com/spf13/viper"
 	"log"
+	"os"
+	"strconv"
 )
 
 type Config struct {
 	Server   ServerConfig   `mapstructure:",squash"`
 	Database DatabaseConfig `mapstructure:",squash"`
-	Redis    RedisConfig    `mapstructure:",squash"`
 	JWT      JWTConfig      `mapstructure:",squash"`
-	SMTP     SMPTConfig     `mapstructure:",squash"`
+	SMTP     SMTPConfig     `mapstructure:",squash"`
 }
 
 type ServerConfig struct {
@@ -21,17 +22,11 @@ type DatabaseConfig struct {
 	DSN string `mapstructure:"DATABASE_DSN"`
 }
 
-type RedisConfig struct {
-	Addr     string `mapstructure:"REDIS_ADDR"`
-	Password string `mapstructure:"REDIS_PASSWORD"`
-	DB       int    `mapstructure:"REDIS_DB"`
-}
-
 type JWTConfig struct {
 	SecretKey string `mapstructure:"JWT_SECRET_KEY"`
 }
 
-type SMPTConfig struct {
+type SMTPConfig struct {
 	Host     string `mapstructure:"SMTP_HOST"`
 	Port     int    `mapstructure:"SMTP_PORT"`
 	Username string `mapstructure:"SMTP_USERNAME"`
@@ -40,23 +35,97 @@ type SMPTConfig struct {
 }
 
 func LoadConfig() (*Config, error) {
-	viper.SetConfigFile(".env")
 	viper.AutomaticEnv()
 
-	if err := viper.ReadInConfig(); err != nil {
-		log.Printf("Error reading config file: %v", err)
+	port, err := strconv.Atoi(os.Getenv("SMTP_PORT"))
+	if err != nil {
+		log.Printf("Error parsing SMTP_PORT: %v", err)
 		return nil, err
 	}
 
-	log.Println("Config file read successfully")
-
-	var config Config
-	if err := viper.Unmarshal(&config); err != nil {
-		log.Printf("Error unmarshaling config: %v", err)
-		return nil, err
+	config := &Config{
+		Server: ServerConfig{
+			Port: os.Getenv("SERVER_PORT"),
+		},
+		Database: DatabaseConfig{
+			DSN: os.Getenv("DATABASE_DSN"),
+		},
+		JWT: JWTConfig{
+			SecretKey: os.Getenv("JWT_SECRET"),
+		},
+		SMTP: SMTPConfig{
+			Host:     os.Getenv("SMTP_HOST"),
+			Port:     port,
+			Username: os.Getenv("SMTP_USERNAME"),
+			Password: os.Getenv("SMTP_PASSWORD"),
+			From:     os.Getenv("SMTP_FROM"),
+		},
 	}
 
 	log.Printf("Loaded configuration: %+v", config)
 
-	return &config, nil
+	return config, nil
 }
+
+//package config
+//
+//import (
+//	"github.com/spf13/viper"
+//	"log"
+//)
+//
+//type Config struct {
+//	Server   ServerConfig   `mapstructure:",squash"`
+//	Database DatabaseConfig `mapstructure:",squash"`
+//	Redis    RedisConfig    `mapstructure:",squash"`
+//	JWT      JWTConfig      `mapstructure:",squash"`
+//	SMTP     SMPTConfig     `mapstructure:",squash"`
+//}
+//
+//type ServerConfig struct {
+//	Port string `mapstructure:"SERVER_PORT"`
+//}
+//
+//type DatabaseConfig struct {
+//	DSN string `mapstructure:"DATABASE_DSN"`
+//}
+//
+//type RedisConfig struct {
+//	Addr     string `mapstructure:"REDIS_ADDR"`
+//	Password string `mapstructure:"REDIS_PASSWORD"`
+//	DB       int    `mapstructure:"REDIS_DB"`
+//}
+//
+//type JWTConfig struct {
+//	SecretKey string `mapstructure:"JWT_SECRET_KEY"`
+//}
+//
+//type SMPTConfig struct {
+//	Host     string `mapstructure:"SMTP_HOST"`
+//	Port     int    `mapstructure:"SMTP_PORT"`
+//	Username string `mapstructure:"SMTP_USERNAME"`
+//	Password string `mapstructure:"SMTP_PASSWORD"`
+//	From     string `mapstructure:"SMTP_FROM"`
+//}
+//
+//func LoadConfig() (*Config, error) {
+//	viper.SetConfigFile(".env")
+//	viper.AutomaticEnv()
+//
+//	if err := viper.ReadInConfig(); err != nil {
+//		log.Printf("Error reading config file: %v", err)
+//		return nil, err
+//	}
+//
+//	log.Println("Config file read successfully")
+//
+//	var config Config
+//	if err := viper.Unmarshal(&config); err != nil {
+//		log.Printf("Error unmarshaling config: %v", err)
+//		return nil, err
+//	}
+//
+//	log.Printf("Loaded configuration: %+v", config)
+//
+//	return &config, nil
+//}
